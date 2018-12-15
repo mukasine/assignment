816a3e1509be3d99bfaf01360a9fbd03247378f6
ingabire <ingabire@sylvie.gmail.com>
2018-12-15 12:07:58 +0200

    liking javaScripf and html

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var a=parseInt(document.getElementById("one").value);
+    var b=parseInt(document.getElementById(two).value);
+    var c=parseInt(document.getElementById(three).value);
+    var text;
+    
+} 
